2531646bafccf3764d7a24ba59103776686535ea
Piotr Brudny <pbrudny@gmail.com>
2020-06-03 19:04:53 +0200

    Add Open Weather API

--- src/features/weather/WeatherContainer.js
+++ src/features/weather/WeatherContainer.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
+import { Input } from 'antd';
+import { Space } from 'antd';
+import weather from 'openweather-apis';
+const { Search } = Input;
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 class WeatherContainer extends Component {
   constructor() {
     super();
+    this.getWeather = this.getWeather.bind(this)
   }
 
   componentDidMount() {
+    weather.setLang('pl');
   }
 
+  getWeather(city) {
+    weather.setCity(city);
+    console.log('API KEY: ', API_KEY);
+    weather.setAPPID(API_KEY);
+    weather.getTemperature(function(err, temp){
+      console.log(temp);
+    });
+  }
   render() {
-    return <h1>Hello</h1>
+    return (
+      <Space>
+        <Search
+          placeholder="podaj miasto"
+          enterButton="Szukaj"
+          size="large"
+          onSearch={value => this.getWeather(value)}
+        />
+      </Space>
+    )
   }
 }
 
