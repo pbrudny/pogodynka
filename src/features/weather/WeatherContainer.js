8acaa082ec81e377f06f9a36bf5138becb88c617
Piotr Brudny <pbrudny@gmail.com>
2020-06-03 21:39:06 +0200

    Wrap api call into promise

--- src/features/weather/WeatherContainer.js
+++ src/features/weather/WeatherContainer.js
@@ -32,8 +32,8 @@ class WeatherContainer extends Component {
 
   constructor() {
     super();
-    this.getWeather = this.getWeather.bind(this);
-    this.cities = this.cities.bind(this);
+    this.setWeather = this.setWeather.bind(this);
+    this.mainCity = this.mainCity.bind(this);
   }
 
   componentDidMount() {
@@ -41,31 +41,53 @@ class WeatherContainer extends Component {
     weather.setAPPID(API_KEY);
     const city = this.props.match.params.city;
     if (city) {
-      this.getWeather(city);
+      this.setWeather(city);
     }
   }
 
-  getWeather(city) {
-    const result = weather.setCity(city);
-    console.log('result', result);
-    weather.getWeatherForecast( (err, obj) => {
-      this.setState({cityForecast: obj})
-    });
+  getWeatherForecast(city) {
+    return new Promise((resolve, reject) => {
+      weather.getWeatherForecast( (err, obj) => {
+        if (err) reject(err);
+        resolve(obj)
+      })
+    })
   }
 
-  // setCityForecast(city) {
-  //   this.setState({cityForecast: this.getWeather(city)})
-  // }
+  async setWeather(city) {
+    weather.setCity(city);
+    const forecast = await this.getWeatherForecast();
+    this.setState({cityForecast: forecast});
+  }
 
-  cities() {
+  getOtherCities(city) {
+    const otherCities = mainCities.filter((value) => value !== city);
+    this.setState({mainCitiesForecast: otherCities.map(city => {
+      weather.setCity(city);
+      weather.getWeatherForecast( (err, obj) => {
+      });
+    })})
+  }
+
+  hourlyWeather(list) {
+    return list.slice(0, 5).map(el => {
+      const {main, wind, weather, dt_txt} = el;
+      return (
+        <p>{dt_txt}: {weather[0].description} {main.temp} °C  (Ciśnienie: {main.pressure}hpa, Wiatr: {wind.speed}m/s)</p>
+      )
+    })
+  }
+
+  mainCity() {
     const forecast = this.state.cityForecast;
 
     if (forecast && forecast.cod === '200') {
       const city = forecast.city.name;
-      const {main, wind, weather}= forecast.list[0];
+
       return (
-        <Card title={city} style={{ width: 500 }}>
-         <p>{weather[0].description} {main.temp} °C  (Ciśnienie: {main.pressure}hpa, Wiatr: {wind.speed}m/s)</p>
+        <Card title={city} style={{ width: 650 }}>
+          {console.log(forecast.list)}
+          {this.hourlyWeather(forecast.list)}
         </Card>
       )
     } else if (forecast && forecast.cod === '404') {
@@ -78,12 +100,12 @@ class WeatherContainer extends Component {
         <Col span={8} offset={8}>
         <Space direction="vertical" size="large" style={{paddingTop: '20px'}}>
           <Search
-            placeholder="podaj miasto"
+            placeholder="Podaj miasto np. Warszawa"
             enterButton="Szukaj"
             size="large"
-            onSearch={value => this.getWeather(value)}
+            onSearch={value => this.setWeather(value)}
           />
-          {this.cities()}
+          {this.mainCity()}
         </Space>
         </Col>
       </Row>
